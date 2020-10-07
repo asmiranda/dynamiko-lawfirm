e98ddc1e2707be87a732adb85e041b60e12bf518
Alex <niq77avie@yahoo.com>
2020-10-07 15:12:47 +0800

    Init

--- beans/Case.js
+++ beans/Case.js
@@ -2,21 +2,82 @@ const mongoose = require('mongoose');
 const { mongooseUtil } = require('../utils/mongooseUtil')
 const { Schema } = mongoose;
 
-const CaseSchema = new Schema({
-    lastName: String,
-    firstName: String,
-    middleInitial: String,
+const PartySchema = new Schema({
     email: String,
-    personType: String,
-    personSubType: String,
-    address: String,
-    phone: String,
-    gender: String,
-    password: String,
-    birthDate: Date,
-    enrollment: EnrollmentSchema,
-    schedules: [SchoolScheduleSchema]
+    name: String,
+    dateAdded: Date,
+    participationType: String,
+    description: String,
+});
+
+const CaseDocumentSchema = new Schema({
+    createdByEmail: String,
+    createdByName: String,
+    dateAdded: Date,
+    effectiveStartDate: Date,
+    effectiveEndDate: Date,
+    description: String,
+});
+
+const MemoSchema = new Schema({
+    createdByEmail: String,
+    createdByName: String,
+    dateAdded: Date,
+    effectiveStartDate: Date,
+    effectiveEndDate: Date,
+    description: String,
+});
+
+const TaskSchema = new Schema({
+    taskDate: Date,
+    title: String,
+    description: String,
+});
+
+const WebLinkSchema = new Schema({
+    addedDate: Date,
+    url: String,
+    title: String,
+    description: String,
+});
+
+const PrescriptionSchema = new Schema({
+    prescribedDate: Date,
+    title: String,
+    description: String,
+});
+
+const DocketSchema = new Schema({
+    addedDate: Date,
+    docketId: String,
+    description: String,
+});
+
+const CaseSchema = new Schema({
+    title: String,
+    status: String,
+    startDate: Date,
+
+    lawyerEmail: String,
+    lawyerName: String,
+    clientEmail: String,
+    clientName: String,
+    description: String,
+    parties: [PartySchema],
+    documents: [CaseDocumentSchema],
+    memos: [MemoSchema],
+    tasks: [TaskSchema],
+    webLinks: [WebLinkSchema],
+    prescriptions: [PrescriptionSchema],
+    dockets: [DocketSchema]
 });
 const Case = mongooseUtil.getConn().model('Case', CaseSchema, 'Case');
 module.exports.Case = Case;
 module.exports.CaseSchema = CaseSchema;
+module.exports.PartySchema = PartySchema;
+module.exports.CaseDocumentSchema = CaseDocumentSchema;
+module.exports.MemoSchema = MemoSchema;
+module.exports.TaskSchema = TaskSchema;
+module.exports.WebLinkSchema = WebLinkSchema;
+module.exports.PrescriptionSchema = PrescriptionSchema;
+module.exports.DocketSchema = DocketSchema;
--- beans/Person.js
+++ beans/Person.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 const { mongooseUtil } = require('../utils/mongooseUtil')
 const { Schema } = mongoose;
-const { SchoolScheduleSchema } = require('./SchoolSchedule')
-
-const EnrollmentSchema = new Schema({
-    dateEnrolled: Date,
-});
 
 const PersonSchema = new Schema({
     lastName: String,
@@ -19,21 +14,9 @@ const PersonSchema = new Schema({
     gender: String,
     password: String,
     birthDate: Date,
-    enrollment: EnrollmentSchema,
-    roles: [{ role: String }],
-    schedules: [SchoolScheduleSchema],
-    modules: [{
-        module: String,
-        code: String,
-        label: String,
-        read: Boolean,
-        create: Boolean,
-        update: Boolean,
-        delete: Boolean
-    }],
+    roles: [{ role: String }]
 });
 const Person = mongooseUtil.getConn().model('Person', PersonSchema, 'Person');
-module.exports.EnrollmentSchema = EnrollmentSchema;
 
 module.exports.Person = Person
 module.exports.PersonSchema = PersonSchema;
